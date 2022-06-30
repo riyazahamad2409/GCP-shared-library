b508bd9ebbd44345d0b22e4c88399aeb1b53b480
riyazahamad2409 <107917224+riyazahamad2409@users.noreply.github.com>
2022-06-30 16:53:43 +0530

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -3,11 +3,9 @@ def call() {
   node {
     
     stage('Build image') {
-      class DbUtils{
         myapp = docker.build("riyazahamadm92/gcp-project:${env.BUILD_ID}")
-        sh "print(myapp)"
-        def save(myapp){...}
-}
+
+
     }
   }
 }
