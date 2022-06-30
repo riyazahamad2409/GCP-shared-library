d74f35d8c04b70aee655bf35f00c93a7525936c7
riyazahamad2409 <107917224+riyazahamad2409@users.noreply.github.com>
2022-06-30 17:33:25 +0530

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,12 +1,15 @@
-class Exam {
- def call() {
+
+def call() {
   node {
    
     stage('Build image') {
         myapp = docker.build("riyazahamadm92/gcp-project:${env.BUILD_ID}")
-        
     }
-
+    stage('Push image') {
+      docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+              myapp.push("latest")
+              myapp.push("${env.BUILD_ID}")
+      }
     }
   }
 }
