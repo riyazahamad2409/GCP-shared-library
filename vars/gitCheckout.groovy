c37ca255eb62c8cf1800be8d2278f62174a45c8b
riyazahamad2409 <107917224+riyazahamad2409@users.noreply.github.com>
2022-06-30 13:32:22 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -5,7 +5,7 @@ def call(GitSCM, Map args = [:], Closure body={}) {
             $class: 'GitSCM',
             branches: [[name: '*/master']],
             extensions: [[$class: 'CleanBeforeCheckout']],
-            userRemoteConfigs: [[credentialsId: 'github_credentials', url: 'https://github.com/riyaz-ahamadm92/${args.repo}.git']],
+            userRemoteConfigs: [[credentialsId: 'github_credentials', url: 'https://github.com/riyaz-ahamadm92/"${args.repo}".git']],
     ])
     body()
 }
