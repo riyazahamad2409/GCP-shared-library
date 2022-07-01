d441a18e0ea4d184278bdc3f5566e681a43c1ba2
riyazahamad2409 <107917224+riyazahamad2409@users.noreply.github.com>
2022-07-01 12:59:37 +0530

    Update deployImageGke.groovy

--- vars/deployImageGke.groovy
+++ vars/deployImageGke.groovy
@@ -2,7 +2,7 @@ def call(Map file = [:]) {
   node {
     stage('Deploy to GKE') {
       sh "sed -i 's/tagversion/${env.BUILD_ID}/g' ${file.name}.yaml"
-      step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: env.DEPLOYMENT_YAML.yaml, credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
+      step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: env.DEPLOYMENT_YAML, credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
     }
   }
 }
