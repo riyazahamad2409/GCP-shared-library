b502ea60f0195ee0112b28e8f7e6a1da7136db37
riyazahamad2409 <107917224+riyazahamad2409@users.noreply.github.com>
2022-07-05 14:55:20 +0530

    Update connectToGke.groovy

--- vars/connectToGke.groovy
+++ vars/connectToGke.groovy
@@ -1,6 +1,6 @@
 def call() {
   
   echo "running kubectl test"
-  sh "gcloud container clusters get-credentials cluster-1 --zone us-central1-c --project mineral-hangar-354512"
-  sh "kubectl get nodes"
+  sh "gcloud container clusters get-credentials cluster-1 --zone env.LOCATION --project env.PROJECT_ID"
+  sh "kubectl get pods"
 }
