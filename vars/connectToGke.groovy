5ab6386cdec219f8498f434b2e80e19289a381a7
riyazahamad2409 <107917224+riyazahamad2409@users.noreply.github.com>
2022-07-05 15:10:57 +0530

    Update connectToGke.groovy

--- vars/connectToGke.groovy
+++ vars/connectToGke.groovy
@@ -3,6 +3,7 @@ def call(Map file = [:]) {
   echo "running kubectl test"
  
   sh "gcloud container clusters get-credentials cluster-1 --zone "env.LOCATION" --project "env.PROJECT_ID""
+  sh "sed -i 's/tagversion/${env.BUILD_ID}/g' ${file.name}.yaml"
   sh "kubectl apply -f ${file.name}.yaml"
   sh "kubectl get pods"
 }
