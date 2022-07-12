aba1bcce7e211fde40f14c67e6598f9bffe822cc
riyazahamad2409 <107917224+riyazahamad2409@users.noreply.github.com>
2022-07-12 13:53:06 +0530

    Update buildAngularApp.groovy

--- vars/buildAngularApp.groovy
+++ vars/buildAngularApp.groovy
@@ -6,11 +6,5 @@ def call() {
         stage("Install node modules") {
             sh "sudo npm install -g npm@8.10.0"
         }
-        stage("Build App") {
-            sh "sudo npm run build"
-        }
-
-        
-        
     }
 }
